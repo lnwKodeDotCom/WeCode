c0eaade964a7a7955ce0ef300beae2ee001a505e
zarazi <zarazi@hotmail.com>
2015-12-23 09:56:09 +0700

    Refactor route group & logged-in template.

--- both/routes/private.js
+++ both/routes/private.js
@@ -1,6 +1,18 @@
-FlowRouter.route('/private', {
-    name: 'private',
+const privateSection = FlowRouter.group({
+    name: 'private'
+});
+
+privateSection.route('/private', {
+    name: 'privateDemo',
     action() {
         BlazeLayout.render('default', {main: 'privatePage'});
+    }
+});
+
+privateSection.route('/add', {
+    name: 'addPost',
+
+    action() {
+        BlazeLayout.render('default', {main: 'addPostPage'});
     }
 });
