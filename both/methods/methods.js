01b4898bf63dd03b4d71873b16431ebf249b0a2b
zarazi <zarazi@hotmail.com>
2015-12-26 05:46:05 +0700

    Added delete post logic.

--- both/methods/methods.js
+++ both/methods/methods.js
@@ -1,5 +1,12 @@
 Meteor.methods({
     hello(me='there') {
         return `Hello ${me}`;
+    },
+    deletePost(id) {
+        console.log('Delete post for id:', id);
+        //TODO: implement post deletion
+
+        return true;
     }
+
 });
