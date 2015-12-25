a579769b97901f3617d8787f8d1603acceaa79f7
zarazi <zarazi@hotmail.com>
2015-12-25 11:13:21 +0700

    Show draft document on post list only for owner.

--- server/publications/list.js
+++ server/publications/list.js
@@ -1,6 +1,11 @@
-Meteor.publish('list', function() {
+Meteor.publish('list', function(showDraft=false) {
     const
-        _list = List.find(),
+        _non_draft = {$or:[{is_draft:false},{is_draft:{$exists:false}}]},
+        _draft_and_owner = {is_draft:true, owner_id: this.userId},
+        _conditions = showDraft ? {$or:[_non_draft, _draft_and_owner]} : _non_draft;
+
+    const
+        _list = List.find(_conditions),
         list = _list.fetch(),
         writerIds = _.pluck(list, 'owner_id'),
         _users = Meteor.users.find({_id: {$in: writerIds}}, {fields: {_id:1, emails:1}});
